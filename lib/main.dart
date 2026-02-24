66b58a1fd0c751fb6172de217dbe8ba94f06f036
avinash <madash2113@gmail.com>
2026-02-24 12:55:03 -0600

    Register and View nearby screens are ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart'; 
-// 👆 Provider package for global state management
-//import 'screens/splash/splash_screen.dart';
-//import 'features/onboarding/onboarding_screen.dart';
-//import 'screens/auth/login_screen.dart';
+import 'package:provider/provider.dart';
 
+// 👇 Firebase Core
+//import 'package:firebase_core/firebase_core.dart';
+//import 'firebase_options.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 
 import 'core/routes/app_routes.dart';
-import 'controllers/profile_controller.dart'; 
+import 'controllers/profile_controller.dart';
 // 👆 ProfileController will store profile image globally
 
-void main() {
+// ======================================================
+// MAIN ENTRY POINT
+// ======================================================
+void main() async {
+  // ======================================================
+  // REQUIRED FOR ASYNC INITIALIZATION (FIREBASE)
+  // ======================================================
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // ======================================================
+  // FIREBASE INITIALIZATION
+  // This connects your app to:
+  // - Firestore
+  // - Firebase Auth
+  // - Future cloud services
+  // ======================================================
+  //await Firebase.initializeApp(
+   // options: DefaultFirebaseOptions.currentPlatform,
+  //);
+  // 🔥 Anonymous sign-in so every device has a UID
+//await FirebaseAuth.instance.signInAnonymously();
+
+
   runApp(
     // ===========================
     // GLOBAL PROVIDER WRAPPER
@@ -22,6 +44,9 @@ void main() {
   );
 }
 
+// ======================================================
+// ROOT APPLICATION
+// ======================================================
 class MySportsApp extends StatelessWidget {
   const MySportsApp({super.key});
 
